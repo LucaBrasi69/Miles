f8509290bc9ae34a3d3c69148eb62b8a80e74ab5
LucaBrasi69 <animal2009@mail.ru>
2022-03-09 19:38:55 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //входные данные
+        int price = 1245;
+        int amount = 20;
+        //логика
+        int bonus = price / amount;
+        System.out.println(bonus);
+
+
+    }
+}
